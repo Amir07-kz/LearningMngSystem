7fb88638bd2a081269a0a13da2b8a25ac60be2fc
Amir07-kz <a.dzhunushodzhaev@gmail.com>
2024-02-18 19:01:58 +0300

    Добавил начальную локализацию + аутентификацию + немного поправил регистрацию

---
+++ app/Http/Controllers/Auth/AuthentificationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Dflydev\DotAccessData\Data;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthentificationController extends Controller
+{
+    public function index()
+    {
+        return view('authentification');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('home');
+        }
+
+        return back()->withErrors([
+            'email' => 'I can\'t find your account',
+        ]);
+    }
+
+    // Выход из системы
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
--- app/Models/Authentification.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class Authentification
-{
-    // переопределить потом для другого
-}
